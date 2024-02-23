2a74e2acbd2f864fa4bb4ba40572d5754809a78c
LeslieC09 <142622243+LeslieC09@users.noreply.github.com>
2024-02-23 11:50:09 -0800

    5.2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+    public void UpdateScore(int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+
+        }
+    }
+}
